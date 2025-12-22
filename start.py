95f9cd0d4ed1f6f7c99d549c215899a4ac73a00e
Maicon Douglas <maicondouglas.contato222@gmail.com>
2025-12-22 17:53:19 -0300

    Adiciona interação com usuário e lógica de idade

--- start.py
+++ start.py
@@ -1,3 +1,22 @@
 # olá esse é o inicio do meu desenvolvimento em pyhton.
 
-print("Olá eu me chamo Maicon e estou começando o meu projeto em python")
+print("Olá eu me chamo Maicon, atualmente estou com 20 anos e estou começando o meu projeto em python")
+
+nome=input("digite seu nome: ")
+idade=int(input("digite sua idade: "))
+
+if idade < 18:
+    print(f"Olá, {nome}! Vejo que você está bem novo ainda ")
+elif idade < 20:
+    print(f"Olá, {nome}! Vejo que você está na flor da idade ")
+elif idade < 35:
+    print(f"Olá, {nome}! Vejo que você está na sua fase adulta")
+elif idade < 40:
+    print(f"Olá, {nome}! Vejo que você está no auge da maturidade")
+    
+
+
+
+
+
+
